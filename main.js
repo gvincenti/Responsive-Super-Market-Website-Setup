b391b867ae539d24907e523f58ef3b38d798bacd
Gonzalo Vincenti <48342050+gvincenti@users.noreply.github.com>
2024-06-27 02:25:17 +0000

    versionchat

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
-const menuButton = document.getElementById("menu-btn");
-const navigationLinks = document.getElementById("nav-links");
-const menuButtonIcon = menuButton.querySelector("i");
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
 
-// Navigation Bar Toggle
-menuButton.addEventListener("click", (e) => {
-  navigationLinks.classList.toggle("open");
-
-  const isOpen = navigationLinks.classList.contains("open");
-  menuButtonIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-2-line");
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
 });
 
-navigationLinks.addEventListener("click", (e) => {
-  navigationLinks.classList.remove("open");
-  menuButtonIcon.setAttribute("class", "ri-menu-2-line");
-});
-
-// Scroll Reveal Animation Effect
